36218d533cca95a405006b3efdcc772f4c1c3bde
Javier Gonzales <jagonzacas@hotmail.com>
2018-04-25 11:51:18 +0100

    Change 25/04/2018 at 11:50
    The program appears to run properly with the input samples given but not with a custom input

--- QuestionMarks.java
+++ QuestionMarks.java
@@ -1,67 +1,66 @@
 /*
 check if there are exactly 3 question marks between EVERY pair of two numbers that add up to 10
 */
-import javax.swing.JOptionPane;
-import java.util.ArrayList;
 import java.util.*;
-import java.io.*;
 
-public class QuestionMarksActual{
+public class QuestionMarks{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		//System.out.println(QuestionMarks(s.nextLine()));
 
-		System.out.println(QuestionMarks("arrb7???4xxbl4???eee5"));		//quicker instead of repeating input	-	should return false
+		System.out.println(QuestionMarks("arrb6???4xxbl5???eee5"));			//should return true
+		System.out.println(QuestionMarks("acc?7??sss?3rr1??????5"));		//should return true
+		System.out.println(QuestionMarks("9???1???9???1???9"));				//should return true
+		System.out.println(QuestionMarks("arrb6???4xxbl"));					//should return true
+		System.out.println(QuestionMarks("aa6?9"));							//should return false
+		System.out.println(QuestionMarks("5??aaaaaaaaaaaaaaaaaaa?5?5"));	//should return false
+		System.out.println(QuestionMarks("arrb7???4xxbl4???eee5"));			//should return false
+		System.out.println(QuestionMarks("9???1???9???3???9"));				//should return false but returns true
 	}
 
 	public static String QuestionMarks(String str) {
-		String bool = "false";
-		int index = 0;
-		int count = 0;
-		int num = 0;
-		int first = 0;
-		int second = 0;
-		int sum = 0;
+		char[] myArray;
+		myArray = str.toCharArray();
 
-		for (int i = 0; i < str.length(); i++) {				//iterates the indexes of the String
+		int numTester, sum;
+		boolean condition1 = false;
+		boolean condition2 = true;
 
-			if (Character.isDigit(str.charAt(i))) {				//if the character at index i is a digit
-				index = i;
+		for (int i = 0; i < myArray.length; i++) {
 
-				for (int j = i + 1; j < str.length(); j++) {	//starts the iteration with the following character
+			numTester = myArray[i];
 
-					if (Character.isDigit(str.charAt(j))) {		//if its a digit goes on to the next index
-						num++;
+			if (numTester > 47 && numTester < 58) {
+				for (int j = (i + 1); j < myArray.length; j++) {
+
+					numTester = myArray[j];
+
+					if (numTester > 47 && numTester < 58) {
+
+						if ((Character.getNumericValue(str.charAt(i)) + Character.getNumericValue(str.charAt(j))) == 10) {
+
+							condition1 = true;
+							sum = 0;
+							for (int k = i + 1; k < j; k++) {
+
+								if (myArray[k] == '?') {
+									sum += 1;
+								}
+							}
+							if (sum != 3) {
+								condition2 = false;
+							}
+						}
 						break;
 					}
-					if (str.charAt(j) == '?') {					//if its '?' the count++
-						count++;
-					}
-				}
-				if (count > 0 && count != 3 && num == 1) {		//if there's two numbers together and 1 or two '?' it returns false
-					return "false";
-				}
-				if (count == 3 && num == 1) {					//if there's 3 '?', the moment there's another number it returns truea and restarts the counters
-					bool = "true";
-					count = 0;
-					num = 0;
 				}
 			}
 		}
-
-		return bool;
+		if (condition1 == true && condition2 == true){
+			return "True";
+		}else{
+			return "False";
+		}
 
 	}
 }
-
-//arrb6???4xxbl5???eee5				true
-//acc?7??sss?3rr1??????5			true
-//5??aaaaaaaaaaaaaaaaaaa?5?5		false
-//9???1???9???1???9					true
-//aa6?9								false
-
-//arrb7???4xxbl4???eee5				false	-	but the application reads it as true (its missing the sum = 10)
-
-//arrb6???4xxbl
-
-//first = Character.getNumericValue(str.charAt(i));		returns the int value that the specified Unicode character represents
